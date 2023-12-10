a37c732bed410028fd4919754556ce7fe358a4ba
Mark McDermott <mark@markmcdermott.io>
2023-12-10 14:23:53 -0700

    add stub users_spec to widgets section

--- install.sh
+++ install.sh
@@ -1583,6 +1583,229 @@ RSpec.describe "/widgets", type: :request do
 end
 ~
 EOF
+cat <<'EOF' | spec/requests/users_spec.rb ~
+# frozen_string_literal: true
+require 'rails_helper'
+require 'spec_helper'
+
+RSpec.describe "/users", type: :request do
+  fixtures :users
+  let(:valid_headers) {{ Authorization: "Bearer " + @michael_token }}
+  let(:admin_2_headers) {{ Authorization: "Bearer " + @ryan_token }}
+  let(:invalid_token_header) {{ Authorization: "Bearer xyz" }}
+  let(:poorly_formed_header) {{ Authorization: "Bear " + @michael_token }}
+  let(:user_valid_create_params_mock_1) {{ name: "First1 Last1", email: "one@mail.com", admin: "false", password: "password", avatar: fixture_file_upload("spec/fixtures/files/michael-scott.png", "image/png") }}
+  let(:user_invalid_create_params_email_poorly_formed_mock_1) {{ name: "", email: "not_an_email", admin: "false", password: "password", avatar: fixture_file_upload("spec/fixtures/files/michael-scott.png", "image/png") }}
+  let(:valid_user_update_attributes) {{ name: "UpdatedName" }}
+  let(:invalid_user_update_attributes) {{ email: "not_an_email" }}
+  
+  before :all do
+    @michael_token = token_from_email_password("michaelscott@dundermifflin.com", "password")
+    @ryan_token = token_from_email_password("ryanhoward@dundermifflin.com", "password")
+  end
+
+  before :each do
+    @user1 = users(:michael)
+    avatar1 = fixture_file_upload(Rails.root.join('spec', 'fixtures', 'files', 'michael-scott.png'),'image/png')
+    @user1.avatar.attach(avatar1)
+    @user2 = users(:jim)
+    avatar2 = fixture_file_upload(Rails.root.join('spec', 'fixtures', 'files', 'jim-halpert.png'),'image/png')
+    @user2.avatar.attach(avatar2)
+  end
+
+  describe "GET /index" do
+    context "with valid headers" do
+      it "renders a successful response" do
+        get users_url, headers: valid_headers
+        expect(response).to be_successful
+      end
+
+      it "gets four users" do
+        get users_url, headers: valid_headers
+        expect(JSON.parse(response.body).length).to eq 4
+      end
+
+      it "gets first users' correct details" do
+        get users_url, headers: valid_headers
+        users = JSON.parse(response.body)
+        michael = users.find { |user| user['email'] == "michaelscott@dundermifflin.com" }
+        require 'pry'; binding.pry
+        expect(michael['name']).to eq "Michael Scott"
+        expect(michael['email']).to eq "michaelscott@dundermifflin.com"
+        expect(michael['admin']).to eq true
+        expect(michael['avatar']).to be_kind_of(String)
+        expect(michael['avatar']).to match(/http.*\michael-scott\.png/)
+        expect(michael['password']).to be_nil
+        expect(michael['password_digest']).to be_nil
+      end
+
+      it "gets second users' correct details" do
+        get users_url, headers: valid_headers
+        users = JSON.parse(response.body)
+        jim = users.find { |user| user['email'] == "jimhalpert@dundermifflin.com" }
+        expect(jim['name']).to eq "Jim Halpert"
+        expect(jim['email']).to eq "jimhalpert@dundermifflin.com"
+        expect(jim['admin']).to be_nil or eq false
+        expect(jim['avatar']).to be_kind_of(String)
+        expect(jim['avatar']).to match(/http.*\jim-halpert\.png/)
+        expect(jim['password']).to be_nil
+        expect(jim['password_digest']).to be_nil
+      end
+    end
+
+    context "with invalid headers" do
+      it "renders an unsuccessful response" do
+        get users_url, headers: invalid_token_header
+        expect(response).to_not be_successful
+      end
+    end
+
+  end
+
+  describe "GET /show" do
+    context "with valid headers" do
+      it "renders a successful response" do
+        get user_url(@user1), headers: valid_headers
+        expect(response).to be_successful
+      end
+      it "gets users' correct details" do
+        get user_url(@user1), headers: valid_headers
+        michael = JSON.parse(response.body)
+        expect(michael['name']).to eq "Michael Scott"
+        expect(michael['email']).to eq "michaelscott@dundermifflin.com"
+        expect(michael['admin']).to eq true
+        expect(michael['avatar']).to be_kind_of(String)
+        expect(michael['avatar']).to match(/http.*\michael-scott\.png/)
+        expect(michael['password']).to be_nil
+        expect(michael['password_digest']).to be_nil
+      end
+    end
+    context "with invalid headers" do
+      it "renders an unsuccessful response" do
+        get user_url(@user1), headers: invalid_token_header
+        expect(response).to_not be_successful
+      end
+    end
+  end
+
+  describe "POST /users" do
+    context "with valid parameters" do
+      it "creates a new User" do
+        expect {
+          post users_url, params: user_valid_create_params_mock_1
+        }.to change(User, :count).by(1)
+      end
+
+      it "renders a successful response" do
+        post users_url, params: user_valid_create_params_mock_1
+        expect(response).to be_successful
+      end
+
+      it "sets correct user details" do
+        post users_url, params: user_valid_create_params_mock_1
+        user = User.order(:created_at).last
+        expect(user['name']).to eq "First1 Last1"
+        expect(user['email']).to eq "one@mail.com"
+        expect(user['admin']).to eq(false).or(be_nil)
+        expect(user['avatar']).to be_nil
+        expect(user['password']).to be_nil
+        expect(user['password_digest']).to be_kind_of(String)
+      end
+
+      it "attaches user avatar" do
+        post users_url, params: user_valid_create_params_mock_1
+        user = User.order(:created_at).last
+        expect(user.avatar.attached?).to eq(true)
+      end
+    end
+
+    context "with invalid parameters (email poorly formed)" do
+      it "does not create a new User" do
+        expect {
+          post users_url, params: user_invalid_create_params_email_poorly_formed_mock_1
+        }.to change(User, :count).by(0)
+      end
+    
+      it "renders a 422 response" do
+        post users_url, params: user_invalid_create_params_email_poorly_formed_mock_1
+        expect(response).to have_http_status(:unprocessable_entity)
+      end  
+    end
+  end
+
+  describe "PATCH /update" do
+    context "with valid parameters and headers" do
+
+      it "updates the requested user attribute" do
+        patch user_url(@user1), params: valid_user_update_attributes, headers: valid_headers
+        @user1.reload
+        expect(@user1.name).to eq("UpdatedName")
+      end
+
+      it "doesn't change the other user attributes" do
+        patch user_url(@user1), params: valid_user_update_attributes, headers: valid_headers
+        @user1.reload
+        expect(@user1['email']).to eq "michaelscott@dundermifflin.com"
+        expect(@user1['admin']).to eq true
+        expect(@user1['avatar']).to be_nil
+        expect(@user1['password']).to be_nil
+        expect(@user1['password_digest']).to be_kind_of(String)
+      end
+
+      it "is successful" do
+        patch user_url(@user1), params: valid_user_update_attributes, headers: valid_headers
+        @user1.reload
+        expect(response).to be_successful
+      end
+    end
+
+    context "with invalid parameters but valid headers" do
+       it "renders a 422 response" do
+         patch user_url(@user1), params: invalid_user_update_attributes, headers: valid_headers
+         expect(response).to have_http_status(:unprocessable_entity)
+       end
+    end
+
+    context "with valid parameters but invalid headers" do
+       it "renders a 401 response" do
+         patch user_url(@user1), params: valid_user_update_attributes, headers: invalid_token_header
+         expect(response).to have_http_status(:unauthorized)
+       end
+    end
+
+  end
+
+  describe "DELETE /destroy" do
+    context "with valid headers" do
+      it "destroys the requested user" do
+        expect {
+          delete user_url(@user1), headers: valid_headers
+        }.to change(User, :count).by(-1)
+      end
+
+      it "renders a successful response" do
+        delete user_url(@user1), headers: valid_headers
+        expect(response).to be_successful
+      end
+    end
+
+    context "with invalid headers" do
+      it "doesn't destroy user" do
+        expect {
+          delete user_url(@user1), headers: invalid_token_header
+        }.to change(User, :count).by(0)
+      end
+
+      it "renders a unsuccessful response" do
+        delete user_url(@user1), headers: invalid_token_header
+        expect(response).to_not be_successful
+      end
+    end
+  end
+
+end
+~
+EOF
 rspec
 
 
